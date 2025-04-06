2b5f0449e107e5f0f0efd59a6899cc22dafd2742
mateolafalce <mateolafalce3@gmail.com>
2025-04-06 14:58:36 -0300

    del raw endpoints

--- backend/FastAPI/main.py
+++ backend/FastAPI/main.py
@@ -1,10 +1,6 @@
-# Clase en vídeo: https://youtu.be/_y9qQZXE24A
-
 ### Hola Mundo ###
 
 # Documentación oficial: https://fastapi.tiangolo.com/es/
-
-# Instala FastAPI: pip install "fastapi[all]"
 
 from fastapi import FastAPI
 from routers import products, users, basic_auth_users, jwt_auth_users, users_db
@@ -13,44 +9,18 @@ from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-# Clase en vídeo: https://youtu.be/_y9qQZXE24A?t=12475
 app.include_router(products.router)
 app.include_router(users.router)
-
-# Clase en vídeo: https://youtu.be/_y9qQZXE24A?t=14094
 app.include_router(basic_auth_users.router)
-
-# Clase en vídeo: https://youtu.be/_y9qQZXE24A?t=17664
 app.include_router(jwt_auth_users.router)
-
-# Clase en vídeo: https://youtu.be/_y9qQZXE24A?t=20480
 app.include_router(users_db.router)
 
-# Clase en vídeo: https://youtu.be/_y9qQZXE24A?t=13618
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Url local: http://127.0.0.1:8000/url
 @app.get("/url")
 async def url():
     return {"url": "https://mouredev.com/python"}
-
-@app.get("/", response_class=HTMLResponse)
-async def root():
-    with open("static/index.html", "r", encoding="utf-8") as f:
-        html = f.read()
-    return html
-
-@app.get("/login", response_class=HTMLResponse)
-async def root():
-    with open("static/html/login.html", "r", encoding="utf-8") as f:
-        html = f.read()
-    return html
-
-@app.get("/register", response_class=HTMLResponse)
-async def root():
-    with open("static/html/register.html", "r", encoding="utf-8") as f:
-        html = f.read()
-    return html
     
 # Inicia el server: uvicorn main:app --reload
 # Detener el server: CTRL+C
