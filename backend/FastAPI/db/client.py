895c7196153943e80200543c0212c9ef441c8321
mateolafalce <mateolafalce3@gmail.com>
2025-04-20 11:01:03 -0300

    conectar al Mongo Marcos

--- backend/FastAPI/db/client.py
+++ backend/FastAPI/db/client.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
 # Usen cualqueira que les funcione
-#db_client = MongoClient("mongodb+srv://ifranmarcosdev:cacadegato1@midatabase.4oxhu5c.mongodb.net/?retryWrites=true&w=majority&appName=midatabase").midatabase
-db_client = MongoClient("mongodb+srv://mateolafalce3:6sAIMJ6s292zTHcd@cluster0.m6wvwib.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").midatabase
+db_client = MongoClient("mongodb+srv://ifranmarcosdev:cacadegato1@midatabase.4oxhu5c.mongodb.net/?retryWrites=true&w=majority&appName=midatabase").midatabase
+#db_client = MongoClient("mongodb+srv://mateolafalce3:6sAIMJ6s292zTHcd@cluster0.m6wvwib.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").midatabase
