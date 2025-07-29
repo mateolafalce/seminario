18022344ff144b8371b3ba73c2509888dbfcbc4b
Karga <nicolasdavidojeda@gmail.com>
2025-07-29 19:58:01 -0300

    Footer nuevo
    sin hacer

--- frontend/src/components/common/Footer/Footer.jsx
+++ frontend/src/components/common/Footer/Footer.jsx
@@ -1,2 +1,15 @@
-//teteo aca debera ir el footer (Pie de pagina) 
 import React from 'react';
+
+{
+    return (
+        <h1>Footer</h1>
+    )
+}
+/*
+
+mapa boulevard
+numero de telefono
+que el footer este pegado al fondo siempre
+que aparezca un pelota que se mueva cuando sube y baja la pestania
+
+*/
