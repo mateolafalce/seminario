4e172ae848476415f4852008e418254865439924
mateolafalce <mateolafalce3@gmail.com>
2025-08-27 19:34:40 -0300

    Añadir comentario

--- backend/FastAPI/services/email.py
+++ backend/FastAPI/services/email.py
@@ -4,6 +4,10 @@ import os
 
 load_dotenv()
 
+# Con esto se puede enviar mails
+# 100/dia
+# 3000/mes
+# Gratarola 🤠
 def enviar_email(to: str, subject: str, html: str):
     resend.api_key = os.getenv("RESEND_TOKEN")
     resend_email = os.getenv("RESEND_EMAIL")
