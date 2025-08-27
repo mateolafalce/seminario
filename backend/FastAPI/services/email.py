def310e13dfce0c5359bfaf1ab6028bf2c4d7402
mateolafalce <mateolafalce3@gmail.com>
2025-08-27 19:27:41 -0300

    Añadir email en el .env

--- backend/FastAPI/services/email.py
+++ backend/FastAPI/services/email.py
@@ -6,8 +6,9 @@ load_dotenv()
 
 def enviar_email(to: str, subject: str, html: str):
     resend.api_key = os.getenv("RESEND_TOKEN")
+    resend_email = os.getenv("RESEND_EMAIL")
     resend.Emails.send({
-        "from": "onboarding@resend.dev",
+        "from": resend_email,
         "to": to,
         "subject": subject,
         "html": html
