77ba5cdd5e424f5618a8510716b8755af42fcb78
mateolafalce <mateolafalce3@gmail.com>
2025-08-27 20:10:17 -0300

    Testear token habilitador

--- frontend/src/pages/Habilitado.jsx
+++ frontend/src/pages/Habilitado.jsx
@@ -1,22 +1,27 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import RedirectMessage from '../components/usuarios/MensajeRedireccion';
+import MiToast from "../components/common/Toast/MiToast";
+import { toast } from "react-toastify";
 
 const Habilitado = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
+    toast(
+      <MiToast 
+        mensaje={"Tu cuenta ha sido habilitada\nSerás redirigido a la página de inicio de sesión en 5 segundos..."} 
+        tipo="success" 
+      />
+    );
     const timer = setTimeout(() => {
       navigate('/login');
-    }, 5000); // Redirects after 5 seconds
+    }, 5000);
 
-    return () => clearTimeout(timer); // Cleanup the timer on component unmount
+    return () => clearTimeout(timer);
   }, [navigate]);
 
   return (
-    <div className="enabled-page">
-      <RedirectMessage />
-    </div>
+    <div></div>
   );
 };
 
